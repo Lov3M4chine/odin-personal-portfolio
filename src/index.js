55b2ca3930911db4d78349a4b29e33cf2b30c845
William Alderman <125694577+Lov3M4chine@users.noreply.github.com>
2023-08-10 14:02:58 -0600

    Add selected projects section

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
 import './styles.css';
+import './imgs/placeholder.jpg'
--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
                 loader: 'url-loader',
                 options: {
                     limit: 8192,
-                    name: 'images/[name].[ext]',
+                    name: 'imgs/[name].[ext]',
                 }
             }
         ]
