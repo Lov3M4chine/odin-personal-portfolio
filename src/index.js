178a4c9dae976f0438adddc32cb928b2ea2192a6
William Alderman <125694577+Lov3M4chine@users.noreply.github.com>
2023-08-15 17:13:06 -0600

    Format code with prettier and organize

--- src/index.js
+++ src/index.js
@@ -1,75 +1,78 @@
-import './styles.css';
-import './imgs/placeholder.jpg';
-import './imgs/personal-photo.jpg';
-import './imgs/personal-photo2.jpg';
-import './imgs/personal-photo3.jpg';
-import './imgs/professional-photo.png';
-import './imgs/professional-photo2.jpg';
-import './imgs/razor-top-black.svg';
+import "./styles.css";
+import "./imgs/placeholder.jpg";
+import "./imgs/personal-photo.jpg";
+import "./imgs/personal-photo2.jpg";
+import "./imgs/personal-photo3.jpg";
+import "./imgs/professional-photo.png";
+import "./imgs/professional-photo2.jpg";
+import "./imgs/razor-top-black.svg";
 
+// ——————————————————————————————————————————————————
+// Projects Slide-In
+// ——————————————————————————————————————————————————
 let wasLinkClicked = false;
 let lastScrollTimestamp = 0;
 const staggerDelay = 300;
 
-document.addEventListener('DOMContentLoaded', function() {
-    const projects = document.querySelectorAll('.slide-in-left, .slide-in-right');
-    const links = document.querySelectorAll('a[href^="#"]');
-    
-    function checkSlide() {
-        const currentTime = Date.now();
-        const timeSinceLastScroll = currentTime - lastScrollTimestamp;
-        
-        projects.forEach((project, index) => {
-            const slideInAt = (window.scrollY + window.innerHeight) - project.offsetHeight / 4;
-            const isSomeShown = slideInAt > project.offsetTop;
-            
-            if (isSomeShown) {
-                if (wasLinkClicked || timeSinceLastScroll < staggerDelay) {
-                    setTimeout(() => {
-                        project.classList.add('active');
-                    }, index * staggerDelay);
-                } else {
-                    project.classList.add('active');
-                }
-            }
-        });
-    }
+document.addEventListener("DOMContentLoaded", function () {
+  const projects = document.querySelectorAll(".slide-in-left, .slide-in-right");
+  const links = document.querySelectorAll('a[href^="#"]');
 
-    function updateWasLinkClicked() {
-        wasLinkClicked = true;
-    }
+  function checkSlide() {
+    const currentTime = Date.now();
+    const timeSinceLastScroll = currentTime - lastScrollTimestamp;
 
-    // Initial check to add .active class to already visible projects
-    checkSlide();
-    
-    window.addEventListener('scroll', function() {
-        lastScrollTimestamp = Date.now();
-        debounce(checkSlide)();
+    projects.forEach((project, index) => {
+      const slideInAt =
+        window.scrollY + window.innerHeight - project.offsetHeight / 4;
+      const isSomeShown = slideInAt > project.offsetTop;
+
+      if (isSomeShown) {
+        if (wasLinkClicked || timeSinceLastScroll < staggerDelay) {
+          setTimeout(() => {
+            project.classList.add("active");
+          }, index * staggerDelay);
+        } else {
+          project.classList.add("active");
+        }
+      }
     });
-    
-    links.forEach(link => {
-        link.addEventListener('click', () => {
-            updateWasLinkClicked();
-            setTimeout(checkSlide, 0);
-        });
-    });
+  }
 
+  function updateWasLinkClicked() {
+    wasLinkClicked = true;
+  }
+  
+  checkSlide();
+
+  window.addEventListener("scroll", function () {
+    lastScrollTimestamp = Date.now();
+    debounce(checkSlide)();
+  });
+
+  links.forEach((link) => {
+    link.addEventListener("click", () => {
+      updateWasLinkClicked();
+      setTimeout(checkSlide, 0);
+    });
+  });
 });
 
 // Debounce function to improve performance on scroll
 function debounce(func, wait = 20, immediate = true) {
-    var timeout;
-    return function() {
-        var context = this, args = arguments;
-        var later = function() {
-            timeout = null;
-            if (!immediate) func.apply(context, args);
-        };
-        var callNow = immediate && !timeout;
-        clearTimeout(timeout);
-        timeout = setTimeout(later, wait);
-        if (callNow) func.apply(context, args);
+  var timeout;
+  return function () {
+    var context = this,
+      args = arguments;
+    var later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
     };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
 }
 
 // ——————————————————————————————————————————————————
@@ -77,124 +80,104 @@ function debounce(func, wait = 20, immediate = true) {
 // ——————————————————————————————————————————————————
 
 class TextScramble {
-    constructor(el) {
-      this.el = el
-      this.chars = '!<>-_\\/[]{}—=+*^?#________'
-      this.update = this.update.bind(this)
+  constructor(el) {
+    this.el = el;
+    this.chars = "!<>-_\\/[]{}—=+*^?#________";
+    this.update = this.update.bind(this);
+  }
+  setText(newText) {
+    const oldText = this.el.innerText;
+    const length = Math.max(oldText.length, newText.length);
+    const promise = new Promise((resolve) => (this.resolve = resolve));
+    this.queue = [];
+    for (let i = 0; i < length; i++) {
+      const from = oldText[i] || "";
+      const to = newText[i] || "";
+      const start = Math.floor(Math.random() * 40);
+      const end = start + Math.floor(Math.random() * 40);
+      this.queue.push({ from, to, start, end });
     }
-    setText(newText) {
-      const oldText = this.el.innerText
-      const length = Math.max(oldText.length, newText.length)
-      const promise = new Promise((resolve) => this.resolve = resolve)
-      this.queue = []
-      for (let i = 0; i < length; i++) {
-        const from = oldText[i] || ''
-        const to = newText[i] || ''
-        const start = Math.floor(Math.random() * 40)
-        const end = start + Math.floor(Math.random() * 40)
-        this.queue.push({ from, to, start, end })
-      }
-      cancelAnimationFrame(this.frameRequest)
-      this.frame = 0
-      this.update()
-      return promise
-    }
-    update() {
-      let output = ''
-      let complete = 0
-      for (let i = 0, n = this.queue.length; i < n; i++) {
-        let { from, to, start, end, char } = this.queue[i]
-        if (this.frame >= end) {
-          complete++
-          output += to
-        } else if (this.frame >= start) {
-          if (!char || Math.random() < 0.28) {
-            char = this.randomChar()
-            this.queue[i].char = char
-          }
-          output += `<span class="dud">${char}</span>`
-        } else {
-          output += from
+    cancelAnimationFrame(this.frameRequest);
+    this.frame = 0;
+    this.update();
+    return promise;
+  }
+  update() {
+    let output = "";
+    let complete = 0;
+    for (let i = 0, n = this.queue.length; i < n; i++) {
+      let { from, to, start, end, char } = this.queue[i];
+      if (this.frame >= end) {
+        complete++;
+        output += to;
+      } else if (this.frame >= start) {
+        if (!char || Math.random() < 0.28) {
+          char = this.randomChar();
+          this.queue[i].char = char;
         }
-      }
-      this.el.innerHTML = output
-      if (complete === this.queue.length) {
-        this.resolve()
+        output += `<span class="dud">${char}</span>`;
       } else {
-        this.frameRequest = requestAnimationFrame(this.update)
-        this.frame++
+        output += from;
       }
     }
-    randomChar() {
-      return this.chars[Math.floor(Math.random() * this.chars.length)]
+    this.el.innerHTML = output;
+    if (complete === this.queue.length) {
+      this.resolve();
+    } else {
+      this.frameRequest = requestAnimationFrame(this.update);
+      this.frame++;
     }
   }
-  
-  // ——————————————————————————————————————————————————
-  // Example
-  // ——————————————————————————————————————————————————
-  
-  const phrases = [
-    'Disciplined',
-    'Versatile',
-    'Dedicated',
-    'Eager to learn',
-    'Eager to teach',
-    'Precise',
-    'Punctual',
-    'Diligent',
-    'Detail-Oriented',
-    'Meticulous',
-    'Personable',
-    'Ridiculously Good-looking'
-  ]
-  
-  const el = document.querySelector('.scramble')
-  const fx = new TextScramble(el)
-  
-  let counter = 0
-  const next = () => {
-    fx.setText(phrases[counter]).then(() => {
-      setTimeout(next, 2800)
-    })
-    counter = (counter + 1) % phrases.length
+  randomChar() {
+    return this.chars[Math.floor(Math.random() * this.chars.length)];
   }
-  
-  next()
+}
 
-  let burger = document.getElementById('burger');
-  let nav = document.getElementById('main-nav');
-  
-  // Existing burger button click event listener
-  burger.addEventListener('click', function(e){
-      this.classList.toggle('is-open');
-      nav.classList.toggle('is-open');
+const phrases = [
+  "Disciplined",
+  "Versatile",
+  "Dedicated",
+  "Eager to learn",
+  "Eager to teach",
+  "Precise",
+  "Punctual",
+  "Diligent",
+  "Detail-Oriented",
+  "Meticulous",
+  "Personable",
+  "Ridiculously Good-looking",
+];
+
+const el = document.querySelector(".scramble");
+const fx = new TextScramble(el);
+
+let counter = 0;
+const next = () => {
+  fx.setText(phrases[counter]).then(() => {
+    setTimeout(next, 2800);
   });
-  
-  // Get all the navigation links
-  let navLinks = document.querySelectorAll('.nav-link');
-  
-  // Attach a click event listener to each navigation link
-  navLinks.forEach(function(link) {
-      link.addEventListener('click', function(e) {
-          
-          // Trigger a click event on the burger button
-          setTimeout(function() {
-            
-            // Trigger a click event on the burger button after the delay
-            burger.click();
+  counter = (counter + 1) % phrases.length;
+};
 
-        }, 800);
-  
-          // If you want to prevent the default action of the link (e.g., jumping to the anchor), 
-          // you can uncomment the following line.
-          // e.preventDefault();
-      });
+next();
+
+// ——————————————————————————————————————————————————
+// Mobile Navigation
+// ——————————————————————————————————————————————————
+
+let burger = document.getElementById("burger");
+let nav = document.getElementById("main-nav");
+let navLinks = document.querySelectorAll(".nav-link");
+
+burger.addEventListener("click", function (e) {
+  this.classList.toggle("is-open");
+  nav.classList.toggle("is-open");
+});
+
+navLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    setTimeout(function () {
+      burger.click();
+    }, 800);
   });
-  
-
-
-
-
-
-
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,27 @@
 module.exports = {
-    purge: [
-      './src/**/*.html',
-      './src/**/*.js',
-    ],
-    theme: {
-      extend: {
-        fontFamily: {
-          Handjet: ['Handjet', 'sans-serif'],
-          Play: ['Play', 'sans-serif'],
-        },
+  purge: ["./src/**/*.html", "./src/**/*.js"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Handjet: ["Handjet", "sans-serif"],
+        Play: ["Play", "sans-serif"],
       },
-      screens: {
-        
-        'phone': '320px',
+    },
+    screens: {
+      phone: "320px",
 
-        'tablet': '640px',
-        // => @media (min-width: 640px) { ... }
-  
-        'laptop': '1024px',
-        // => @media (min-width: 1024px) { ... }
-  
-        'desktop': '1280px',
-        // => @media (min-width: 1280px) { ... }
-  
-      }
+      tablet: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      laptop: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      desktop: "1280px",
+      // => @media (min-width: 1280px) { ... }
     },
-    variants: {
-      extend: {},
-    },
-    plugins: [],
-  };
-  
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
